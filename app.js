15ea7bda7c079611e0854ea3ad247238d5efcac5
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-15 15:26:38 -0500

    implemented the first step in the encryption took a test string and iterated across it comparing each character to an array of characters returning the index of the character from the array. thereby converting each letter, period, comma and space to a decimal number value

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-const firstCypher = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
-  'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-  'w', 'x', 'y', 'z', '.', ',', ' ' ];
-
-const testString = 'test';
 
 
+const testString = 'this is a test string';
 
+// take a string and map over it comparing the character to the
+//one in the array returning the index of that character
+
+
+
+const firstConversion = function(testString) {
+  const firstCypher = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    'w', 'x', 'y', 'z', '.', ',', ' ' ];
+
+  for (let i = 0; i < testString.length; i++) {
+  
+    //take input and test it against all the elements
+    //in the array returning the index of the character
+    firstCypher.map((character) => {
+      let input = testString.charAt(i)
+      if(character === input) {
+        let index = firstCypher.indexOf(character)
+        console.log(index)
+        return
+      }
+    })
+  }
+}
+
+firstConversion(testString)
