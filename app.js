d3aa2a21402210af62677f31fad55f49dd6bdeca
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-15 14:52:26 -0500

    created an array that includes the entire alphabet in lower case and a comma, period and space. I will later use this array to map over the string that is input and return the index of the letter or character.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hi')
+const firstCypher = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+  'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+  'w', 'x', 'y', 'z', '.', ',', ' ' ];
+
+const testString = 'test';
+
+
+
