b347907685fe6cb7292c4e400e4ed58a72c6b7e9
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-15 16:01:45 -0500

    cleaned up the code removing three superfulous variables

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-
-
 const testString = 'test';
 
 // take a string and map over it comparing the character to the
 //one in the array returning the index of that character
-
-
 
 const firstConversion = function(testString) {
   const firstCypher = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
@@ -13,20 +9,30 @@ const firstConversion = function(testString) {
     'w', 'x', 'y', 'z', '.', ',', ' ' ];
   let decimalConversion = [];
   for (let i = 0; i < testString.length; i++) {
-  
     //take input and test it against all the elements
     //in the array returning the index of the character
     firstCypher.map((character) => {
-      let input = testString.charAt(i)
-      if(character === input) {
-        let index = firstCypher.indexOf(character)
-        decimalConversion.push(index)
+      if(character === testString.charAt(i)) {
+        decimalConversion.push(firstCypher.indexOf(character));
       }
-    })
-  }
+    });
+  };
   return decimalConversion;
-}
+};
 
-const firstEncryption = firstConversion(testString)
+const firstEncryption = firstConversion(testString);
 
-console.log(firstEncryption)
+//take the first encryption array and convert it to a quaternary array
+//is there a quarternary notation in javascript>?
+
+const secondConversion = function(firstEncryption) {
+  let quarterConversion = [];
+  firstEncryption.map((chars)  => {
+    quarterConversion.push(chars.toString(4))
+  });
+  return quarterConversion;
+};
+
+const yourEncryptedString = secondConversion(firstEncryption)
+
+console.log(yourEncryptedString);
